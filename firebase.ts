0b63aec032f932e9bd43a2833a459307e649d9a6
valigeriagioberti-glitch <valigeriagioberti@gmail.com>
2026-01-15 09:21:40 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAuir_9-oi6qn29lAJbF7y3EF7NEDBUAPw",
+  authDomain: "luggage-deposit-dashboard.firebaseapp.com",
+  projectId: "luggage-deposit-dashboard",
+  storageBucket: "luggage-deposit-dashboard.firebasestorage.app",
+  messagingSenderId: "746584809132",
+  appId: "1:746584809132:web:ba192efb8ebac668dcc909",
 };
+
 
 const app = initializeApp(firebaseConfig);
 
